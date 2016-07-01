11422b1208d95d33707a52e5e91f88631b8414e2
jdsutton <jdsutton@calpoly.edu>
2016-07-01 14:36:37 -0700

    Set up initial tileset editor

--- server.py
+++ server.py
@@ -1,7 +1,33 @@
+'''
+    This file contains code for the server
+    It serves static files
+    It also accepts new/updated files
+'''
+
+from os import listdir
 import os.path
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, render_template
 
 app = Flask(__name__)
+
+TILESET_DIRECTORY = "img/tiles"
+
+class Tileset():
+    def __init__(self, fileName, size=32, xoff=0, yoff=0):
+        self.fileName = fileName
+        self.size = size
+        self.xoff = xoff
+        self.yoff = yoff
+
+def getAllTilesets():
+    tilesets = []
+
+    files = listdir(TILESET_DIRECTORY)
+    for file in files:
+        if file.endswith(".png"):
+            tilesets.append(Tileset(file))
+
+    return tilesets
 
 @app.route("/")
 def showHomepage():
@@ -19,5 +45,23 @@ def sendCode(path):
 def sendGamesFile(path):
     return send_from_directory("games", path)
 
+@app.route('/'  + TILESET_DIRECTORY + '/<path:path>')
+def sendTileset(path):
+    return send_from_directory(TILESET_DIRECTORY, path)
+
+@app.route('/tilesets')
+def showTilesets():
+    return render_template("tilesets.html",
+        tilesets=getAllTilesets())
+
+@app.route('/tilesets/<fileName>/edit')
+def editTileset(fileName):
+    tilesets = getAllTilesets()
+    for i in xrange(len(tilesets)):
+        if tilesets[i].fileName == fileName:
+            return render_template("tileset_editor.html",
+                tilesetDirectory=TILESET_DIRECTORY,
+                tileset=tilesets[i])
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
